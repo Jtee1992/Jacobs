e9538a03874c609c68964e12c441fe5733336442
Jacob T <Jacob.t.thompson1@gmail.com>
2017-11-20 16:28:05 -0800

    Understanding Multi-Dimenstional arrays

---
+++ Understanding Mulitdimensional Arrays/Understanding Mulitdimensional Arrays/Default.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Understanding_Mulitdimensional_Arrays
+{
+    public partial class Default : System.Web.UI.Page
+    {
+
+        double[,] priceGrid;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //double[,] priceGrid = new double[3, 3];
+            // 0 - Chicago
+            // 1 - New York
+            // 2 - London
+            priceGrid = new double[3, 3];
+            priceGrid[0, 1] = 350.0;
+            priceGrid[0, 2] = 750.0;
+            priceGrid[1, 0] = 400.0;
+            priceGrid[1, 2] = 700d;
+            priceGrid[2, 0] = 800d;
+            priceGrid[2, 1] = 805d;
+
+            resultLabel.Text = priceGrid[1, 2].ToString();
+
+
+        }
+
+       
+
+        protected void okButton_Click(object sender, EventArgs e)
+        {
+            int fromCity;
+            if (fromChicagoRadio.Checked) fromCity = 0;
+            else if (fromNewYorkRadio.Checked) fromCity = 1;
+            else fromCity = 2;
+            
+        }
+    }
+}
---
+++ Understanding Mulitdimensional Arrays/Understanding Mulitdimensional Arrays/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Understanding_Mulitdimensional_Arrays.Global" Language="C#" %>
