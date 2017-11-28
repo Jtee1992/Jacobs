c2dbdbf6b58f76d27f6f9a05291cfcc487f5c5ac
Jacob T <Jacob.t.thompson1@gmail.com>
2017-11-28 11:35:19 -0800

    Looping with the While and Do ststaments

---
+++ Looping with the For Iteration Statement/Looping with the For Iteration Statement/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Looping_with_the_For_Iteration_Statement.Global" Language="C#" %>
---
+++ Looping with the For Iteration Statement/Looping with the For Iteration Statement/Global.asax.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace Looping_with_the_For_Iteration_Statement
+{
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+        }
+    }
+}
